51f31d58576673b7447f9f9199625a0ea72acaa9
Darshanpro123 <darshanpro187@gmail.com>
2024-10-22 10:01:17 +0530

    Add style to edit form color and size

--- 2nd_Redux_FormExample/src/App.jsx
+++ 2nd_Redux_FormExample/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
 import DatePicker from "react-datepicker";
@@ -15,7 +15,12 @@ function App() {
   } = useForm();
   const dispatch = useDispatch();
   const entries = useSelector(selectEntries);
-  const [date, setDate] = React.useState(null);
+  const [date, setDate] = useState(null);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleShowPassword = () => {
+    setShowPassword((prev) => !prev);
+  };
 
   const handleChange = (date) => {
     setDate(date);
@@ -44,53 +49,78 @@ function App() {
     <div className="flex flex-col items-center  text-gray-50">
       <form
         onSubmit={handleSubmit(handleData)}
-        className="flex w-11/12 md:w-6/12 flex-col gap-4 border border-gray-400 p-6 bg-gray-800 rounded-lg shadow-lg"
+        className="flex w-11/12 md:w-6/12 gap-8 flex-col   p-6  rounded-lg "
       >
-        <h1 className="text-2xl text-center mb-4">User Registration Form</h1>
-
+        <h1 className="text-4xl font-bold text-blue-900 text-center mb-4">
+          User Registration Form
+        </h1>
+        <label className="text-gray-800 text-xl" htmlFor="name">
+          Name:
+        </label>
         <input
           {...register("name", { required: "Name is required" })}
           type="text"
           placeholder="Name"
-          className={`p-2 bg-gray-700 rounded-lg ${
+          className={`p-2 h-14 bg-gray-700 rounded-lg ${
             errors.name ? "border border-red-500" : ""
           }`}
         />
         {errors.name && (
           <span className="text-red-500">{errors.name.message}</span>
         )}
-
+        <label className="text-gray-800 text-xl" htmlFor="email">
+          Email:
+        </label>
         <input
           {...register("email", { required: "Email is required" })}
           type="email"
           placeholder="Email"
-          className={`p-2 bg-gray-700 rounded-lg ${
+          className={`p-2 h-14 bg-gray-700 rounded-lg ${
             errors.email ? "border border-red-500" : ""
           }`}
         />
         {errors.email && (
           <span className="text-red-500">{errors.email.message}</span>
         )}
-
-        <input
-          {...register("password", { required: "Password is required" })}
-          type="password"
-          placeholder="Password"
-          className={`p-2 bg-gray-700 rounded-lg ${
-            errors.password ? "border border-red-500" : ""
-          }`}
-        />
+        <label className="text-gray-800 text-xl" htmlFor="password">
+          Password:
+        </label>
+        <div
+          className="
+        relative flex justify-between items-center
+        "
+        >
+          {" "}
+          <input
+            {...register("password", { required: "Password is required" })}
+            type={showPassword ? "text" : "password"}
+            id="password-field"
+            placeholder="Password"
+            className={`p-2 h-14 bg-gray-700 w-5/6 rounded-lg ${
+              errors.password ? "border border-red-500" : ""
+            }`}
+          />
+          <span
+            toggle="#password-field"
+            className="field-icon text-xl cursor-pointer"
+            onClick={handleShowPassword}
+          >
+            {showPassword ? "🙈" : "👁️"}
+          </span>
+        </div>
         {errors.password && (
           <span className="text-red-500">{errors.password.message}</span>
         )}
-
+        <label className="text-gray-800 text-xl" htmlFor="date">
+          Select Date Of Birth:
+        </label>
         <div className="relative">
           <DatePicker
             {...register("birthDate", { required: "Birth date is required" })}
             selected={date}
             startDate={new Date("1900-01-01")}
             onChange={handleChange}
-            className={`p-2 bg-gray-700 rounded-lg ${
+            className={`p-2 h-14 bg-gray-700 rounded-lg ${
               errors.birthDate ? "border border-red-500" : ""
             }`}
             dateFormat="MMMM d, yyyy"
@@ -100,8 +130,10 @@ function App() {
             <span className="text-red-500">{errors.birthDate.message}</span>
           )}
         </div>
-
-        <div className="flex gap-4 bg-gray-700 p-2 rounded-lg w-full">
+        <label className="text-gray-800 text-xl" htmlFor="">
+          Gendar:
+        </label>
+        <div className="flex h-14 gap-4 bg-gray-700 p-2 rounded-lg w-full">
           <label htmlFor="field-male" className="flex items-center">
             <input
               {...register("gender", { required: "Gender is required" })}
@@ -126,11 +158,10 @@ function App() {
         {errors.gender && (
           <span className="text-red-500">{errors.gender.message}</span>
         )}
-
         <input
           type="submit"
           value="Submit"
-          className="border border-gray-700 bg-orange-400 hover:bg-orange-700 cursor-pointer text-gray-50 px-4 py-2 rounded transition duration-200"
+          className="border h-14 border-gray-700 bg-orange-400 hover:bg-orange-700 cursor-pointer text-gray-50 px-4 py-2 rounded transition duration-200"
         />
       </form>
 
